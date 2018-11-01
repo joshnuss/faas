9f10cdf4ebec89ee2ec04a6fd6837e5e3a7d57d0
Josh Nussbaum <joshnuss@gmail.com>
2018-10-31 21:52:11 -0400

    Adjusts formatting

--- faas_web/lib/faas_web_web/controllers/call_controller.ex
+++ faas_web/lib/faas_web_web/controllers/call_controller.ex
@@ -1,38 +1,39 @@
- defmodule FaasWebWeb.CallController do
-   use FaasWebWeb, :controller
-   alias FaasWeb.{Function, Call}
+defmodule FaasWebWeb.CallController do
+  use FaasWebWeb, :controller
+  alias FaasWeb.{Function, Call}
 
-   def create(conn, params = %{"name" => name}) do
-     case Repo.get_by(Function, name: name) do
-       nil ->
-         conn
-         |> put_status(:not_found)
-         |> render(FaasWebWeb.ErrorView, "404.json")
+  def create(conn, params = %{"name" => name}) do
+    case Repo.get_by(Function, name: name) do
+      nil ->
+        conn
+        |> put_status(:not_found)
+        |> render(FaasWebWeb.ErrorView, "404.json")
 
-       function ->
-         attrs = %{
-           function_id: function.id,
-           params: params["params"] || []
-         }
-         changeset = Call.changeset(%Call{}, attrs)
+      function ->
+        attrs = %{
+          function_id: function.id,
+          params: params["params"] || []
+        }
 
-         {:ok, call} = Repo.insert(changeset)
+        changeset = Call.changeset(%Call{}, attrs)
 
-         conn
-         |> put_status(:created)
-         |> render("show.json", call)
-     end
-   end
+        {:ok, call} = Repo.insert(changeset)
 
-   def show(conn, params = %{"id" => id}) do
-     case Repo.get_by(Call, id: id) do
-       nil ->
-         conn
-         |> put_status(:not_found)
-         |> render(FaasWebWeb.ErrorView, "404.json")
+        conn
+        |> put_status(:created)
+        |> render("show.json", call)
+    end
+  end
 
-       call ->
-         render(conn, "show.json", call)
-     end
-   end
- end
+  def show(conn, params = %{"id" => id}) do
+    case Repo.get_by(Call, id: id) do
+      nil ->
+        conn
+        |> put_status(:not_found)
+        |> render(FaasWebWeb.ErrorView, "404.json")
+
+      call ->
+        render(conn, "show.json", call)
+    end
+  end
+end
