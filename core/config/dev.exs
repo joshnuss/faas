8534c1da2a1f11452c13a7ee1bcf7c40edd33df8
Josh Nussbaum <joshnuss@gmail.com>
2018-11-06 01:19:51 -0500

    Fix dev config

--- core/config/dev.exs
+++ core/config/dev.exs
@@ -1,9 +1,9 @@
 use Mix.Config
 
 # Configure your database
-#config :faas_core, Faas.Core.Repo,
-#  username: "postgres",
-#  password: "postgres",
-#  database: "faas_dev",
-#  hostname: "localhost",
-#  pool_size: 10
+config :faas_core, Faas.Core.Repo,
+  username: "postgres",
+  password: "postgres",
+  database: "faas_dev",
+  hostname: "localhost",
+  pool_size: 10
