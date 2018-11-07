eb64cf0289f6d616824e4becd1a2526f890a1ec5
Josh Nussbaum <joshnuss@gmail.com>
2018-11-07 14:02:18 -0500

    Uses route helper in function view

--- web/lib/web/views/function_view.ex
+++ web/lib/web/views/function_view.ex
@@ -22,7 +22,7 @@ defmodule Faas.Web.FunctionView do
       id: function.id,
       name: function.name,
       code: function.code,
-      path: "/$/#{function.name}",
+      path: Routes.function_path(Endpoint, :show, function.name),
       inserted_at: function.inserted_at,
       updated_at: function.updated_at
     }
