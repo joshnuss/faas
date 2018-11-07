b00fcb52fa4f60b63ac3260d5944a83bf0f10534
Josh Nussbaum <joshnuss@gmail.com>
2018-11-07 03:17:17 -0500

    Adds worker as temporary dep in web

--- web/mix.exs
+++ web/mix.exs
@@ -39,7 +39,8 @@ defmodule Faas.Web.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:faas_core, path: "../core"}
+      {:faas_core, path: "../core"},
+      {:faas_worker, path: "../worker"}
     ]
   end
 
