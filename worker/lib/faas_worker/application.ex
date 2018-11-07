28033652b9507523b3bf557839cf9ecaf2c8ee2e
Josh Nussbaum <joshnuss@gmail.com>
2018-11-07 02:10:46 -0500

    Switches worker to dynamic supervisor

--- worker/lib/faas_worker/application.ex
+++ worker/lib/faas_worker/application.ex
@@ -6,15 +6,9 @@ defmodule Faas.Worker.Application do
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
-    children = [
-      # Starts a worker by calling: Faas.Worker.Worker.start_link(arg)
-      # {Faas.Worker.Worker, arg},
-    ]
-
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Faas.Worker.Supervisor]
-    Supervisor.start_link(children, opts)
+    DynamicSupervisor.start_link(opts)
   end
 end
